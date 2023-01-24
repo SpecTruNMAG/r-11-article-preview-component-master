b36af02608588301bd138693277ddbf35d3eb9ca
Miguel Alarcon <refugionet@gmail.com>
2023-01-24 13:59:22 -0500

    v1 complete

--- javascript.js
+++ javascript.js
@@ -1,8 +1,23 @@
 const share = document.querySelector(".share");
 const profileLinks= document.querySelector(".profile-links");
 
-profileLinks.addEventListener("click",() => share.style.display = "flex");
-profileLinks.addEventListener("mouseleave",() => time=(setTimeout(() => share.style.display = "none",500)));
+
+profileLinks.addEventListener("click",() => {
+    share.style.display = "flex"
+    profileLinks.style.background = "url(./images/icon-share-white.svg)no-repeat center center";
+    profileLinks.style.backgroundColor="hsl(214, 17%, 51%)";
+});
+profileLinks.addEventListener("mouseleave",() => time=(setTimeout(() => {
+    share.style.display = "none";
+    profileLinks.style.background = "url(./images/icon-share.svg)no-repeat center center";
+    profileLinks.style.backgroundColor="hsl(210, 46%, 95%)";
+    
+},500)));
 
 share.addEventListener("mouseenter", () => clearTimeout(time));
-share.addEventListener("mouseleave",() => setTimeout(()=>share.style.display="none",500));
+
+share.addEventListener("mouseleave",() => setTimeout(()=>{
+    share.style.display="none";
+    profileLinks.style.background = "url(./images/icon-share.svg)no-repeat center center";
+    profileLinks.style.backgroundColor="hsl(210, 46%, 95%)";
+},500));
